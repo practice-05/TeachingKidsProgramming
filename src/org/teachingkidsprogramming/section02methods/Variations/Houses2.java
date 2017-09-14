b431051f02a6308c3cf11bc2eae08282885320cb
practice-05 <91086387@g.mvusd.net>
2017-09-14 14:31:57 -0700

    Houses Variations

---
+++ src/org/teachingkidsprogramming/section02methods/Variations/Houses2.java
@@ -0,0 +1,48 @@
+package org.teachingkidsprogramming.section02methods.Variations;
+
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.virtualproctor.VirtualProctor;
+
+public class Houses2
+{
+  public static void main(String[] args)
+  {
+    VirtualProctor.setClassName("Palm Digital");
+    VirtualProctor.setName("Giselle");
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+    Tortoise.setX(200);
+    int height = 40;
+    drawHouse(height);
+    drawHouse(120);
+    drawHouse(90);
+    drawHouse(20);
+  }
+  private static void drawHouse(int height)
+  {
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.move(height);
+    // drawFlatRoof();
+    //draw a pointy roof
+    drawPointyRoof();
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawPointyRoof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(15);
+    Tortoise.turn(90);
+    Tortoise.move(15);
+    Tortoise.turn(45);
+  }
+  private static void drawFlatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+  }
+}
---
+++ src/org/teachingkidsprogramming/section02methods/Variations/Houses3.java
@@ -0,0 +1,56 @@
+package org.teachingkidsprogramming.section02methods.Variations;
+
+import org.teachingextensions.logo.Tortoise;
+import org.teachingextensions.logo.utils.ColorUtils.PenColors;
+import org.teachingextensions.virtualproctor.VirtualProctor;
+
+public class Houses3
+{
+  public static void main(String[] args)
+  {
+    VirtualProctor.setClassName("Palm Digital");
+    VirtualProctor.setName("Giselle");
+    Tortoise.show();
+    Tortoise.setSpeed(10);
+    Tortoise.setX(200);
+    int height = 40;
+    drawHouse(height);
+    drawHouse(120);
+    drawHouse(90);
+    drawHouse(20);
+  }
+  private static void drawHouse(int height)
+  {
+    Tortoise.setPenColor(PenColors.Reds.Red);
+    Tortoise.move(height);
+    // drawFlatRoof();
+    //draw a pointy roof
+    //drawPointyRoof();
+    //draw a trapezoid
+    Tortoise.turn(45);
+    Tortoise.move(7.5);
+    Tortoise.turn(0);
+    Tortoise.move(15);
+    Tortoise.turn(45);
+    Tortoise.move(7.5);
+    Tortoise.turn(45);
+    Tortoise.move(height);
+    Tortoise.turn(-90);
+    Tortoise.move(20);
+    Tortoise.turn(-90);
+  }
+  private static void drawPointyRoof()
+  {
+    Tortoise.turn(45);
+    Tortoise.move(15);
+    Tortoise.turn(90);
+    Tortoise.move(15);
+    Tortoise.turn(45);
+  }
+  private static void drawFlatRoof()
+  {
+    Tortoise.turn(90);
+    Tortoise.move(30);
+    Tortoise.turn(90);
+  }
+}
