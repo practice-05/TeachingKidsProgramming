e3dc81c0266f6eb25c3f91bcc23e58e8bd3576e8
practice-05 <91086387@g.mvusd.net>
2017-09-19 09:53:55 -0700

    Started Triangle Shell

--- src/org/teachingkidsprogramming/section02methods/TriangleShell.java
+++ src/org/teachingkidsprogramming/section02methods/TriangleShell.java
@@ -1,12 +1,14 @@
 package org.teachingkidsprogramming.section02methods;
 
+import org.teachingextensions.logo.Tortoise;
+
 @SuppressWarnings("unused")
 public class TriangleShell
 {
   private static int length = 1;
   public static void main(String[] args)
   {
-    // Show the tortoise --#1
+    Tortoise.show();
     // Make the tortoise go as fast as possible --#6
     // Do the following 60 times --#7.1
     //      Change the pen color of the line the tortoise draws to a random color --#9
@@ -14,10 +16,11 @@ public class TriangleShell
     //      drawTriangle (recipe below) --#5.1
     //
     //      ------------- Recipe for drawTriangle --#5.2
-    //          Do the following 3 times --#3.1
-    //              Move the tortoise using the current length --#4
-    //              Turn the tortoise 1/3rd of 360 degrees --#2
-    //          End Repeat --#3.2
+    for (int i = 0; i < 3; i++)
+    {
+      Tortoise.move(length);
+      Tortoise.turn(360 / 3);
+    }
     //      ------------- End of drawTriangle recipe --#5.3
     //
     //      Turn the tortoise 1/60th of 360 degrees to the right --#10
