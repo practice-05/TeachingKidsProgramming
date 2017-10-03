7c31438ae1c4350c38daf17cd7e240c999240930
practice-05 <91086387@g.mvusd.net>
2017-10-03 09:59:51 -0700

    Started

--- src/org/teachingkidsprogramming/section02methods/SequentialExecution01.java
+++ src/org/teachingkidsprogramming/section02methods/SequentialExecution01.java
@@ -2,4 +2,32 @@ package org.teachingkidsprogramming.section02methods;
 
 public class SequentialExecution01
 {
+  static int counter = 1;
+  //Looking at sequential execution,
+  //    the order operations run in a program
+  public static void main(String[] args)
+  {
+    System.out.println("In main #1");
+    myMethod1();
+    System.out.println("In main #2");
+  }
+  public static void myMethod1()
+  {
+    System.out.println("In myMethod1 #1");
+    myMethod2();
+  }
+  public static void myMethod2()
+  {
+    System.out.println("In myMethod2 #1");
+    if (counter < 10)
+    {
+      counter++;
+      myMethod3();
+    }
+  }
+  public static void myMethod3()
+  {
+    System.out.println("In myMethod3 #1");
+    myMethod2();
+  }
 }
